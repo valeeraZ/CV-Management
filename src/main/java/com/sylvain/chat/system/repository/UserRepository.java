d8825134bec4a6460a0f2befa8865db497d72460
Wenzhuo Zhao <zslyvain@gmail.com>
2020-08-11 02:55:36 +0200

    Update: Repository CRUD

---
+++ src/main/java/com/sylvain/chat/system/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.sylvain.chat.system.repository;
+
+import com.sylvain.chat.system.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByUsername(String username);
+
+    @Modifying
+    @Transactional(rollbackFor = Exception.class)
+    void deleteByUsername(String username);
+
+    boolean existsByUsername(String username);
+}
---
+++ src/main/java/com/sylvain/chat/system/repository/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package com.sylvain.chat.system.repository;
+
+import com.sylvain.chat.system.entity.UserRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+}
