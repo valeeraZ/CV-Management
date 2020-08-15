e853e07adc0156917a4bc21d07d64b6ab2c180f2
Wenzhuo Zhao <zslyvain@gmail.com>
2020-08-16 01:59:10 +0200

    NEW: user service for register

---
+++ src/main/java/com/sylvain/chat/system/service/UserService.java
@@ -0,0 +1,64 @@
+package com.sylvain.chat.system.service;
+
+import com.google.common.collect.ImmutableMap;
+import com.sylvain.chat.system.DTO.UserRegisterDTO;
+import com.sylvain.chat.system.entity.Role;
+import com.sylvain.chat.system.entity.User;
+import com.sylvain.chat.system.entity.UserRole;
+import com.sylvain.chat.system.enums.RoleType;
+import com.sylvain.chat.system.exception.RoleNotFoundException;
+import com.sylvain.chat.system.exception.UsernameAlreadyExistsException;
+import com.sylvain.chat.system.exception.UsernameNotFoundException;
+import com.sylvain.chat.system.repository.RoleRepository;
+import com.sylvain.chat.system.repository.UserRepository;
+import com.sylvain.chat.system.repository.UserRoleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @_(@Autowired))
+public class UserService {
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final UserRoleRepository userRoleRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    /**
+     * register a user with role USER
+     * if exception occurs, roll back this transaction
+     * @param userRegisterDTO the request from controller
+     */
+    @Transactional(rollbackFor = Exception.class)
+    public void save(UserRegisterDTO userRegisterDTO){
+        usernameNotExists(userRegisterDTO.getUsername());
+        User user = userRegisterDTO.toUser();
+        user.setPassword(bCryptPasswordEncoder.encode(userRegisterDTO.getPassword()));
+        userRepository.save(user);
+        Role userRole = roleRepository.findByName(RoleType.USER.getName()).orElseThrow(()->new RoleNotFoundException(ImmutableMap.of("rolename",RoleType.USER.getName())));
+        userRoleRepository.save(new UserRole(user,userRole));
+    }
+
+    /**
+     * find an user by his username
+     * @param username
+     * @return the user object
+     */
+    public User find(String username){
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(ImmutableMap.of("username",username)));
+    }
+
+    /**
+     * ensure the username does not exist in database
+     * if exists, throw an exception
+     * @param username
+     */
+    private void usernameNotExists(String username){
+        boolean exist = userRepository.findByUsername(username).isPresent();
+        if(exist)
+            throw new UsernameAlreadyExistsException(ImmutableMap.of("username",username));
+    }
+}
