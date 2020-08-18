7228a237a190f2697813f291b39f5693c966b415
Wenzhuo Zhao <zslyvain@gmail.com>
2020-08-18 02:33:50 +0200

    NEW: JSON Web Token generator

--- src/test/java/com/sylvain/chat/ChatApplicationTests.java
+++ src/test/java/com/sylvain/chat/ChatApplicationTests.java
@@ -1,8 +1,13 @@
 package com.sylvain.chat;
 
+import com.sylvain.chat.security.utils.JWTTokenUtils;
+import com.sylvain.chat.system.enums.RoleType;
 import com.sylvain.chat.system.exception.ErrorCode;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class ChatApplicationTests {
@@ -12,8 +17,19 @@ class ChatApplicationTests {
     }
 
     @Test
-    void validationMessages(){
+    void testValidationMessagesInternational(){
         System.out.println("ErrorCode.USER_NAME_ALREADY_EXISTS.getMessage() = " + ErrorCode.USERNAME_ALREADY_EXISTS.getMessage());
+    }
+    
+    @Test
+    void testJWT(){
+        String username = "David";
+        List<String> authorities = new ArrayList<>();
+        authorities.add(RoleType.USER.getName());
+        boolean rememberMe = true;
+        
+        String token = JWTTokenUtils.createToken(username,authorities,rememberMe);
+        System.out.println("token = " + token);
     }
 
 }
