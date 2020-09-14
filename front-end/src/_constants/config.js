293a176a2def606950fc663c9c7b3df03f7bb2a1
Wenzhuo Zhao <zslyvain@gmail.com>
2020-09-14 17:37:15 +0200

    FIX: fornt end's fetch url of server for production build

--- front-end/src/_constants/config.js
+++ front-end/src/_constants/config.js
@@ -1,1 +1,5 @@
-export const URL = "http://localhost:8080";
+//For development, use localhost:8080 to connect with server
+//export const URL = "http://localhost:8080";
+
+//For production, the Front End runs at the same port with server (8080)
+export const URL = "";
