4cb9a0135b44ca32512c38c4fc16bc9575711218
valeeraZ <zslyvain@gmail.com>
2020-09-18 13:11:36 +0200

    FIX: try to fix Azure's Bean Dependency cycle about @Autowired userService

--- src/main/java/com/sylvain/chat/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/sylvain/chat/security/service/UserDetailsServiceImpl.java
@@ -2,22 +2,31 @@ package com.sylvain.chat.security.service;
 
 import com.sylvain.chat.security.entity.AuthUser;
 import com.sylvain.chat.system.entity.User;
-import com.sylvain.chat.system.service.UserService;
+import com.sylvain.chat.system.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
-    private final UserService userService;
+    /**Decouple relation of userDetailServiceImpl -> userService
+    /*private final UserService userService;
 
     public UserDetailsServiceImpl(UserService userService) {
         this.userService = userService;
+    }*/
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username)  {
-        User user = userService.find(username);
+        User user = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("username.notfound")
+        );
         return new AuthUser(user);
     }
 }
