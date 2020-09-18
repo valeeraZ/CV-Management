c1db45b0873465dd1172c6137b6d3a13e0846789
valeeraZ <zslyvain@gmail.com>
2020-09-18 02:40:19 +0200

    FIX: try to fix Azure's Bean Dependency cycle about @Autowired userService

--- src/main/java/com/sylvain/chat/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/sylvain/chat/security/service/UserDetailsServiceImpl.java
@@ -3,16 +3,17 @@ package com.sylvain.chat.security.service;
 import com.sylvain.chat.security.entity.AuthUser;
 import com.sylvain.chat.system.entity.User;
 import com.sylvain.chat.system.service.UserService;
-import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
-
     private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username)  {
