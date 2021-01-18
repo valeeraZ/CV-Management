633990bb83c156de72b0fb259e3c8dcfba580ec7
valeeraZ <zslyvain@gmail.com>
2021-01-18 19:24:16 +0100

    UPDATE: new GitHub CI; front end proxy url

--- front-end/src/_constants/config.js
+++ front-end/src/_constants/config.js
@@ -1,5 +1,5 @@
 //For development, use localhost:8080 to connect with server
-export const URL = "http://localhost:8080";
+//export const URL = "http://localhost:8080";
 
 //For production, the Front End runs at the same port with server (8080)
-//export const URL = "https://chat-web.azurewebsites.net/";
+export const URL = "http://64.225.1.187:8080";
